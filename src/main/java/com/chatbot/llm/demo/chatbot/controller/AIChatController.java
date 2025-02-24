0b76658967448d7b85e62eb329a4ee3b9765975c
vishant <vishantbhole@gmail.com>
2025-02-23 21:37:30 -0600

    [build] - AI chatbot using NVDIA LLM

---
+++ src/main/java/com/chatbot/llm/demo/chatbot/controller/AIChatController.java
@@ -0,0 +1,35 @@
+package com.chatbot.llm.demo.chatbot.controller;
+
+import java.util.Map;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import reactor.core.publisher.Flux;
+
+@RestController
+public class AIChatController {
+
+    private final ChatClient chatClient;
+
+    @Autowired
+    public AIChatController(ChatClient.Builder builder) {
+        this.chatClient = builder.build();
+    }
+
+    @GetMapping("/ai/generate")
+    public String generate(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
+        String response = chatClient.prompt().user(message).call().content();
+        return response;
+    }
+
+    @GetMapping("/ai/generateStream")
+    public Flux<String> generateStream(
+            @RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
+        return chatClient.prompt().user(message).stream().content();
+    }
+
+}
